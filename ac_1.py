1333ded863d762353a07963f887974a2c8cf4d14
Guilherme Resende da Rocha <202402075365@alunos.ibmec.edu.br>
2024-02-28 22:28:33 -0300

    Update ac_1.py

--- ac_1.py
+++ ac_1.py
@@ -13,7 +13,7 @@ a = int(input("Informe o parâmetro a da equação: "))
 b = int(input("Informe o parâmetro b da equação: "))
 c = int(input("Informe o parâmetro c da equação: "))
 
-# # Calcula e apresenta as raizes da equação
+# Calcula e apresenta as raizes da equação
 
 print("A primeira raiz da equação é: ", (-b + ((b ** 2) - 4 * a * c) ** 0.5) / (2 * a))
 print("A segunda raiz da equação é: ", (-b - (b ** 2 - 4 * a * c) ** 0.5) / (2 * a))
@@ -32,4 +32,4 @@ ano = int(input("Informe o ano: "))
 
 # Um ano bissexto deve ser múltiplo de 4, mas anos múltiplos de 100 que não são múltiplos de 400 não são bissextos.
 
-print(bool(ano % 4 == 0 and (not ano % 100 == 0 or ano % 400 == 0)))
+print(ano % 4 == 0 and (not ano % 100 == 0 or ano % 400 == 0))
