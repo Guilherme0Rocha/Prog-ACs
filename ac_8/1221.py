ea56b707a8afb1789bf353271ac099fbeba3e4f1
Guilherme Resende da Rocha <202402075365@alunos.ibmec.edu.br>
2024-05-01 02:20:56 -0300

    Update 1221.py

--- ac_8/1221.py
+++ ac_8/1221.py
@@ -1,22 +1,17 @@
-def check():
-        num = int(input())
-        lista =[2, 3, 4, 5, 6, 7, 8, 9, 10]
-        if num in lista:
-            lista.remove(num)
-        for i in lista:
-            if num % i == 0:
-                return False
+import math
+
+def ePrimo(n):
+    if n%2 == 0 and n>2:
+        return False
+    return all(n%i for i in range(3,int(math.sqrt(n))+1, 2))
 
 def main():
     casos = int(input())
-    while casos > 0:
-        try:
-            if check() == False:
-                print("Not Prime")
-            else:
-                print("Prime")
-        except EOFError:
-            break
-
+    for _ in range(casos):
+        num = int(input())
+        if (ePrimo(num)):
+            print('Prime')
+        else:
+            print('Not Prime')
 
 main()
